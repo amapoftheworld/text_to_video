f4bcb6fec0c17313dd1bf7e78391f65ce7264c45
peroon <fromjpn@gmail.com>
2017-10-13 01:23:27 +0900

    Image inserted video is generated and uploaded to https://www.youtube.com/watch?v=1nNqAd5JDdk

--- text_to_video.py
+++ text_to_video.py
@@ -3,6 +3,7 @@ import textwrap
 import math
 import re
 import glob
+import time
 from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
@@ -50,7 +51,9 @@ def generate_png(s, i):
     img = Image.new('RGBA', IMAGE_SIZE)
     if insert_image_path is not None:
         insert_image = Image.open(insert_image_path)
-
+        insert_image = insert_image.resize(IMAGE_SIZE)
+        print('inset', insert_image_path)
+        img.paste(insert_image)
     draw = ImageDraw.Draw(img)
     font = ImageFont.truetype(FONT_PATH, FONT_SIZE)
     lines = textwrap.wrap(s, width = WORD_NUM_PER_LINE)
@@ -80,6 +83,7 @@ def generate_png(s, i):
     img.save(output_path)
 
 def make_movie(file_name):
+    global insert_image_path
     os.system('rm temp/image/*.png')
     os.system('rm temp/sound/*')
     os.system('rm temp/video/*.mp4')
@@ -100,8 +104,12 @@ def make_movie(file_name):
 
         if text[0] == '[' and text[-1] == ']':
             print('command', i)
-            insert_image_path = text[1:-2]
-            print('path', insert_image_path)
+            path = text[1:-1]
+            if path == 'None':
+                print('None dane')
+                insert_image_path = None
+            else:
+                insert_image_path = path
         else:
             aiff_path = generate_aiff(text, i)
             aiff_to_mp3(aiff_path)
@@ -113,10 +121,13 @@ def make_movie(file_name):
     mp4_path_list = glob.glob('temp/video/*.mp4')
     f = open(text_path, 'w')
     for mp4_path in mp4_path_list:
+        mp4_path = mp4_path.replace('temp/', '')
         f.write('file ' + mp4_path + '\n')
     f.close()
     # concatenate all mp4
-    #os.system('ffmpeg -f concat -i {0} {1}'.format(text_path, 'temp/output/{0}.mp4'.format(file_name)))
+    start_time = time.time()
+    os.system('ffmpeg -f concat -i {0} {1}'.format(text_path, 'temp/output/{0}.mp4'.format(file_name)))
+    print('merge time', time.time() - start_time, 'sec')
 
 if __name__ == '__main__':
     #make_movie('sample.txt')
